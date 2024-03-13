f964ca02590f7b647b5611af8441bb874feaa6c6
rohit8450 <rohitrajthakur10@gmail.com>
2024-03-13 14:36:19 +0530

    first commit

---
+++ src/pages/app/learn/Learn.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import style from "./Learn.module.css";
+import coursesData from "../../../data/courses.json";
+import {Link, useParams} from 'react-router-dom';
+
+// Task4: Import all the required elements from the react-router-dom
+
+function Learn() {
+  const {courseId} = useParams();
+  const course = coursesData.find((course) => course.id === courseId);
+  return (
+    <div className={style.courses_container}>
+      <div className={style.top_head}>
+        {/* Task4: Create Link to go back to the Courses page */}
+        <Link to={`/courses`}>
+        <h2 className={style.back}>{"<<"}</h2>
+        </Link>
+        <h2 className={style.back}>{"<<"}</h2>
+
+        {/* Task4: Title of the Course */}
+        <Link to={`/courses/${courseId}`}>
+        <h1 className={style.heading}>{course.title}</h1>
+        </Link>
+      </div>
+      <div className={style.chapter_box}>
+        <div className={style.chapters}>
+          <h1>Chapters</h1>
+          <hr />
+          <ul>{/*Task4: List of Chapters must be rendered here  */}
+          {course.chapters.map((chap, i) => {
+            return (
+              <div className={style.chapterId} key={i}>
+                <Link to={`chapter/${chap.chapter}`}>
+                {chap.title}
+                </Link>
+                
+              </div>
+            );
+          }
+            
+          )}</ul>
+        </div>
+      
+      <div className={style.courses}>
+        {/**Task5:  Chapter Details Must be rendered here */}
+        <outlet context={{...course}}/>
+      </div>
+    </div>
+</div>
+  );
+}
+
+export default Learn;
